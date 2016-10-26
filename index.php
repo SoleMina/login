86fc69e8aed488cc2ece7298895608bac2c8be87
karina <karina>
2016-10-26 14:53:32 -0500

    Haciendo un login user

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sesiones</title>
+</head>
+<body>
+	<h2>Login User - Php</h2><span>Programando</span>
+	<form method="post" action="datos.php">
+		<h3>Please sign in</h3>
+		<label for="user">User</label>
+		<p><input type="text" name="user" placeholder="User name"></p>
+		<label for="password">Password</label>
+		<p><input type="text" name="password" placeholder="Pasword"></p>
+		<input type="submit" name="submit" value="Sign in">
+	</form>
+</body>
+</html>
